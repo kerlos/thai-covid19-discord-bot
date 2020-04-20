d83b83696068639e461ab1a1661246a2aed5238f
Vatunyoo Suwannapisit <ker.los@hotmail.com>
2020-04-20 16:12:41 +0700

    add error handle for check update embed

--- bot.go
+++ bot.go
@@ -556,7 +556,7 @@ func checkUpdateEmbed(s *discordgo.Session, chID, msgID string, val int) {
 		s.ChannelMessageSend(chID, err.Error())
 	}
 
-	if len(msg.Embeds) > 0 && val != 2 {
+	if msg != nil && msg.Embeds != nil && len(msg.Embeds) > 0 && val != 2 {
 		embed := msg.Embeds[0]
 		if embed.Title == "ตรวจระดับความเสี่ยงและคำแนะนำในการปฏิบัติตน COVID19" {
 			u, _ := url.Parse(embed.URL)
